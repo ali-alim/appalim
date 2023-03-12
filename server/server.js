2d995bf3a424d52a79b05c905affae47853f5261
ali-alim <ali.mamedov.2016@gmail.com>
2023-03-12 09:26:08 +0400

    some fix

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+
 mongoose
 .connect(
   process.env.MONGO_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
   })
 .then(() => console.log("MongoDB connected!"))
 .catch(err => console.log(err));
